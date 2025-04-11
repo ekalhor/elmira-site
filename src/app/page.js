70cd1d5de59e704f3398498269df5e02bd9b61d6
ekalhor <elmira.kalhor@gmail.com>
2025-04-11 10:10:19 -0700

    Centerng the test image

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,9 @@
-'use client';
-
-export default function HomePage() {
-  return (
-    <main className="min-h-screen bg-yellow-100 flex items-center justify-center">
-      <img
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/PNG_transparency_demonstration_1.png/300px-PNG_transparency_demonstration_1.png"
-        alt="Test image"
-        width={300}
-        height={300}
-        className="rounded-full shadow-md"
-      />
-    </main>
-  );
-}
+<div className="w-full flex justify-center">
+  <img
+    src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/PNG_transparency_demonstration_1.png/300px-PNG_transparency_demonstration_1.png"
+    alt="Test image"
+    width={300}
+    height={300}
+    className="rounded-full shadow-md"
+  />
+</div>
