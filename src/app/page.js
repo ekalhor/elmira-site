1fbd60c250df1ad639371ebbe6e827c4d96eb1b1
ekalhor <elmira.kalhor@gmail.com>
2025-04-11 09:35:47 -0700

    Only photo

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,12 @@
 'use client';
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
 
 export default function HomePage() {
   return (
-    <main className={`min-h-screen bg-white text-zinc-800 font-sans leading-relaxed flex items-center justify-center ${inter.variable}`}>
+    <main className={`min-h-screen bg-white flex items-center justify-center ${inter.variable}`}>
       <Image
         src="/IMG_1032.JPG"
         alt="Elmira Kalhor"
